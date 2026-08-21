b78e3d5485852c3114599b9ec362888471e77dbc
Rafhael Maglunob <rafhaelmaglunob02@gmail.com>
2026-08-22 00:35:18 +0800

    refactor: clean files and implement kiosk UI theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Outlet,
 } from "react-router-dom";
-import { AuthGuard } from "./lib/hooks/useAuth";
+
 import { useAuthStore } from "./lib/stores/auth.store";
-import ServerMaintenancePage from "./pages/ServerDown";
+
+import MainLayout from "./pages/MainLayout";
 import LandingPage from "./pages/LandingPage";
+import PlaceReport from "./pages/PlaceReport";
+import ScanningReport from "./pages/ScanningReport";
 
 function App() {
   const initialize = useAuthStore((s) => s.initialize);
@@ -19,33 +21,15 @@ function App() {
   }, [initialize]);
 
   return (
-    <main className="flex h-screen overflow-hidden bg-background text-on-background font-body antialiased">
-      <Router>
-        <Routes>
-          {/* Public routes */}
-          <Route element={<LandingPage />} path="/" />
-          <Route element={<h1>Sign In</h1>} path="/signin" />
-          <Route element={<h1>Sign Up</h1>} path="/signup" />
-          <Route
-            element={<ServerMaintenancePage />}
-            path="/server-maintenance"
-          />
-
-          {/* Protected routes */}
-          <Route
-            element={
-              <AuthGuard>
-                <Outlet />
-              </AuthGuard>
-            }
-          >
-            <Route element={<h1>Dashboard</h1>} path="/dashboard" />
-            <Route element={<h1>Tasks</h1>} path="/tasks" />
-            <Route element={<h1>Settings</h1>} path="/settings" />
-          </Route>
-        </Routes>
-      </Router>
-    </main>
+    <Router>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/place-report" element={<PlaceReport />} />
+          <Route path="/scanning-report" element={<ScanningReport />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
